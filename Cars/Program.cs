35e8711f7944f30409d12dfb5378e67f3d241cec
Anna26100002 <69114344+Anna26100002@users.noreply.github.com>
2022-08-04 21:30:32 +0300

    Update Program.cs

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,10 +11,28 @@ namespace Cars
         static void Main(string[] args)
         {
             Car car = new Car();
-            car.Type = "A";
-            car.Сonsumption = 10;
-            car.VolumeOfTheTank = 55;
-            car.Speed = 80;
+            Console.WriteLine("Тип автомобиля:");
+            car.Type = Console.ReadLine();
+
+            Console.WriteLine("Средний расход топлива:");
+            car.Сonsumption = float.Parse(Console.ReadLine());
+            Console.WriteLine("Объём топливного бака:");
+            car.VolumeOfTheTank = int.Parse(Console.ReadLine());
+            Console.WriteLine("Скорость:");
+            car.Speed = int.Parse(Console.ReadLine());
+
+            float distance = car.Distance(car.VolumeOfTheTank);
+            Console.WriteLine(distance + "км может проехать при полном баке топлива");
+            float reservePower = car.PowerReserve(0, 0);
+            Console.WriteLine("Запас хода: " + reservePower);
+            Console.WriteLine("Количество топлива:");
+            int fuel = int.Parse(Console.ReadLine());
+            int timeInPath = car.TimeInPath(fuel, 100);
+            Console.WriteLine("За " + timeInPath + "ч проедет автомобиль" + "100км");
+            PassengerCar car2 = new PassengerCar();
+            float powerReserve = car2.PowerReserve(2, 0);
+            Console.WriteLine(powerReserve);
+            Console.ReadKey(); 
         }
     }
 }
